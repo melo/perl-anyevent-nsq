a9d7134082f9185b8c41e254ac6c80f711c647d1
Pedro Melo <melo@simplicidade.org>
2014-07-20 22:13:50 +0100

    Add publish() to Write
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/AnyEvent/NSQ/Writer.pm
+++ lib/AnyEvent/NSQ/Writer.pm
@@ -10,5 +10,21 @@ use Carp 'croak';
 
 use parent 'AnyEvent::NSQ::Client';
 
+#### Producer API
+
+## Publish a single message - callback is only called if we succedd
+sub publish {
+  my ($self, $topic, $data, $cb) = @_;
+
+  my $conn = $self->_random_connected_conn;
+  croak "ERROR: there no active connections at this moment," unless $conn;
+
+  $cb = sub { $cb->($self, $topic, $data, @_) }
+    if $cb;
+
+  return $conn->publish($topic, $data, $cb);
+}
+
+
 
 1;
