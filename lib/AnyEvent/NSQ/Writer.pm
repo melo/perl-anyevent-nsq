d3fe82af45356c6474bea6090317ea4ad8d9c952
Pedro Melo <melo@simplicidade.org>
2014-07-20 19:52:48 +0100

    Added skel for our Writer class:
    It connect and identifies just fine, but no API for publishing right now.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ lib/AnyEvent/NSQ/Writer.pm
@@ -0,0 +1,14 @@
+package AnyEvent::NSQ::Writer;
+
+# ABSTRACT: a NSQ.io asynchronous producer
+# VERSION
+# AUTHORITY
+
+use strict;
+use warnings;
+use Carp 'croak';
+
+use parent 'AnyEvent::NSQ::Client';
+
+
+1;
