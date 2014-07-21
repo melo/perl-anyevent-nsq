cfa7a7b4a5366c812cf53b0e2ccdd18c85f397ab
Pedro Melo <melo@simplicidade.org>
2014-07-21 05:16:17 +0100

    Move publish() public API to Client, even Readers get to publish sometimes
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/AnyEvent/NSQ/Writer.pm
+++ lib/AnyEvent/NSQ/Writer.pm
@@ -10,23 +10,5 @@ use Carp 'croak';
 
 use parent 'AnyEvent::NSQ::Client';
 
-#### Producer API
-
-## Publish a single or multiple message - callback is only called if we succedd
-sub publish {
-  my ($self, $topic, @data) = @_;
-
-  my $conn = $self->_random_connected_conn;
-  croak "ERROR: there no active connections at this moment," unless $conn;
-
-  if (ref($data[-1]) eq 'CODE' or !defined($data[-1])) {
-    my $cb = pop @data;
-    push @data, sub { $cb->($self, $topic, \@data, @_) }
-      if $cb;
-  }
-
-  return $conn->publish($topic, @data);
-}
-
 
 1;
